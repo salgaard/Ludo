99a006d4b2ed34ea317c6371fa4f95a8809c0c1a
Seymen Kiran <seki48835@edu.ucl.dk>
2025-03-06 12:26:09 +0100

    Added moveservice Tests (not finished)

---
+++ LudoTest/MoveServiceTests/MoveServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using LudoAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoTest.MoveServiceTests
+{
+
+    public class MoveServiceTest
+    {
+
+        public void MoveService_KickPieceHome_PieceShouldBeHome()
+        {
+            //Arrange
+            MoveService moveService = new();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
