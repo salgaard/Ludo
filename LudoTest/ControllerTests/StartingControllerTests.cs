edcb2526689de479c8c3f9e32741cc4ce0e82fdc
Christian Salgård Svinding <142394086+cssv56187@users.noreply.github.com>
2025-03-14 12:48:43 +0100

    cooked some controller tests

---
+++ LudoTest/ControllerTests/StartingControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LudoAPI.Controllers;
+using LudoAPI.Models;
+using LudoAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoTest.ControllerTests
+{
+    public class StartingControllerTests
+    {
+        private readonly Mock<IStartingService> _startingServiceMock;
+        private readonly StartingController _controller;
+
+        public StartingControllerTests()
+        {
+            _startingServiceMock = new Mock<IStartingService>();
+            _controller = new StartingController(_startingServiceMock.Object);
+        }
+
+        [Fact]
+        public void GetStartingRoll_ShouldReturnStartingRoll()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
